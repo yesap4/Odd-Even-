6866575de3e6b732a3c1f0c40c3dad548fab2249
Anant Prakash <yes4ap@gmail.com>
2026-01-01 05:25:10 +0000

    Implement basic functionality for Odd or Even number checker

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+# Import Modules
+import streamlit as st
 
+# Page Header
+
+st.set_page_config(page_title='Odd Or Even?')
+
+
+# Page Title
+
+st.title('Odd or Even? ')
+
+# Description 
+
+st.info('You Can Enter Any Number and know, Whether It is Odd OR Even.')
+
+
+# Taking Input
+# We will add min_value at the last because if we don't add it the program will take input in decimal value
+a = st.number_input('Enter Any Number and know, Whether It is Odd Or Even.',min_value=0)
+if a % 2 == 0:
+    st.success(f'{a} is Even Number')
+else:
+    st.success(f'{a} is an Odd Number')
